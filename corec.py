ee16bc0cb490bbc3292271f7539359c5d1d43945
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-12-22 19:11:51 +0100

    Update corec.py

--- corec.py
+++ corec.py
@@ -15,7 +15,20 @@ BOT_PREFIX = ("Corec,", "Corec, ", "C", "Corec!", "C,")
 
 client = Bot(command_prefix=BOT_PREFIX)
 c2 = discord.Client()
+@client.event
+async def on_ready():
+    await client.change_presence(game=Game(name="Use James,commands"))
+    print("Logged in as " + client.user.name)
 
+async def list_servers():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        print("Current servers:")
+        for server in client.servers:
+            print(server.name)
+        await asyncio.sleep(600)
+        
+        
 
 
 
