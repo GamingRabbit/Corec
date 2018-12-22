70d31f7b8b1cacd0a879b9c4720a56b049e8f272
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-12-22 19:10:46 +0100

    Create corec.py

---
+++ corec.py
@@ -0,0 +1,17 @@
+import random
+import asyncio
+import aiohttp
+from discord import Game
+from discord.ext.commands import Bot
+import discord
+from discord.ext import commands
+import os
+import sys
+import traceback
+
+
+BOT_PREFIX = ("Corec,", "Corec, ", "C", "Corec!", "C,")
+
+
+client = Bot(command_prefix=BOT_PREFIX)
+c2 = discord.Client()
